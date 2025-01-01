919927fe770545416cf28120b2787e4104d03de7
vedantsinggh <vedantsinggh@gmail.com>
2025-01-02 05:21:10 +0530

    quick fix static ip

--- server/server.js
+++ server/server.js
@@ -19,5 +19,5 @@ app.use('/api/task', taskRoutes);
 app.use('/api/group', groupRoute);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT , "172.16.39.134", () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT ,() => console.log(`Server running on port ${PORT}`));
 
